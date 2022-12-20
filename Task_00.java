89996048195fac9d974b6626b85f67e336a16f6b
MaxX <33282787+m1644@users.noreply.github.com>
2022-12-20 16:33:48 +0300

    Add Task_00

---
+++ Task_00.java
@@ -0,0 +1,11 @@
+/**
+ * Task_00 (hashCode)
+ */
+public class Task_00 {
+
+    public static void main(String[] args) {
+        Integer a = 123;
+        System.out.println(a.hashCode());
+        System.out.println("z".hashCode());
+    }
+}
