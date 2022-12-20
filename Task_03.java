f8005ee0721dadd8aac82593c8fc55fa02d0d80b
MaxX <33282787+m1644@users.noreply.github.com>
2022-12-20 16:34:29 +0300

    Add Task_03

---
+++ Task_03.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Task_03 (HashMap Как ускорить работу)
+ */
+public class Task_03 {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map1 = new HashMap<>();
+        Map<Integer, String> map2 = new HashMap<>(9);
+        Map<Integer, String> map3 = new HashMap<>(9, 1.0f);
+    }
+}
